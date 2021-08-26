d608e02bd0acc7817eeddd438490b9e1a73e0e8c
Rodric <72948934+RodricBr@users.noreply.github.com>
2021-08-26 20:31:44 -0300

    Update secret.sh

--- secret.sh
+++ secret.sh
@@ -48,12 +48,20 @@ if [[ "$senha" == "r0dricbr" ]]; then
   echo -e "\n\t${VERDE}[+] Bem vindo,${FIM} $USER!\n"
   while true; do
     echo -e "\n<============== ${AZUL}M${VERDE}e${AMARELO}n${CYANO}u${FIM} ==============>"
-    echo -e "\n\t${VERMELHO}[!] Menu de Opções:${FIM}\n\t ${AZUL}1 Diretório Atual\n\t 2 ID do Usuário\n\t 3 Maior arquivo\n\t 0 Sair da aplicação${FIM}"
+    echo -e "\n\t${VERMELHO}\
+ [!] Menu de Opções:${FIM}\n\t\
+ ${AZUL}1 Diretório Atual\n\t\
+ 2 ID do Usuário\n\t\
+ 3 Maior arquivo\n\t\
+ 4 Processos em execução\n\t\
+ 0 Sair da aplicação${FIM}"
     read -rp "Sua escolha: " opcao_menu #-r para evitar quebrar/bugar o código
+    #du -a $opcao_dir | sort -n -r | head -n 10 antigo
     case "$opcao_menu" in
           1) echo -e "" ; ls -la --color ; echo -e "\n${CYANO}Caminho:${FIM} " $(pwd) ;;
           2) echo -e "${CYANO}Info:${FIM} " $(id) ;;
-          3) read -rp "Diretório: " opcao_dir ; echo -e "\n${AMARELO}Tamanho | Arquivo\n   V         V${FIM}" ; du -a $opcao_dir | sort -n -r | head -n 10 ;;
+          3) read -rp "Diretório: " opcao_dir ; echo -e "\n${AMARELO}Tamanho | Arquivo\n   V         V${FIM}" ; du $opcao_dir -aBM | sort -nr | head -n 10 | more ;;
+          4) echo -e "" ; ps ;;
           0) echo -e "${VERMELHO}Finalizando...${FIM}" ; exit 0 ;;
     esac
     echo -e "\n<==================================>\n"
