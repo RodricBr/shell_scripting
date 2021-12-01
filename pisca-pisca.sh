f775ad9c72d0542fc0d9745bacd44b323122687c
Rodric <72948934+RodricBr@users.noreply.github.com>
2021-12-01 20:17:10 -0300

    Create pisca-pisca.sh

---
+++ pisca-pisca.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/bash
+
+# Texto com cor que fica piscando
+
+# Branco
+echo -e "\033[37;5mRodricBr\033[0m"
+
+# Ciano
+echo -e "\033[36;5mRodricBr\033[0m"
+
+# Magenta
+echo -e "\033[35;5mRodricBr\033[0m"
+
+# Azul
+echo -e "\033[34;5mRodricBr\033[0m"
+
+# Amarelo
+echo -e "\033[33;5mRodricBr\033[0m"
+
+# Verde
+echo -e "\033[32;5mRodricBr\033[0m"
+
+# Vermelho
+echo -e "\033[31;5mRodricBr\033[0m"
+
+# Preto
+echo -e "\033[30;5mRodricBr\033[0m"
+
+
+#### Diferenciados
+#                    Texto        Resetar cores
+# Itálico piscando   VVVVVVVV     V
+echo -e "\033[33;3;5mRodricBr\033[0m"
+              ^^ ^ ^
+              |  | Piscar
+              |  |
+             Cor Itálico
