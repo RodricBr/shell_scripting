7422b2276ac50bcc938e9e09c37a6cb51496a033
Rodric <72948934+RodricBr@users.noreply.github.com>
2022-03-18 19:00:36 -0300

    Update pisca-pisca-e-cores.sh

--- pisca-pisca-e-cores.sh
+++ pisca-pisca-e-cores.sh
@@ -4,7 +4,7 @@
 #### Funciona apenas para xterm
 
 # Especificações de texto com cor no BASH:  \e[<ESTILO>;<COR>m
-# (Nota: \e == BASH || \033 == OCT || \x1b == HEX )
+# (Nota: \e == BASH || \033 == OCT || \x1b == HEX || 27 DEC )
 # Estilos:  0=normal, 1=negrito, 2=escurecido, 4=sublinhado, 5=piscando(xterm), 7=destacado
 # Cores: 31=red, 32=green, 33=yellow, 34=blue, 35=purple, 36=cyan, 37=white
 
