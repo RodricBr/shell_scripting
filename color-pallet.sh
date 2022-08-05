5bf790b5f7c47dd6f72bd1fd6164db3431dde420
Rodric <72948934+RodricBr@users.noreply.github.com>
2022-08-04 23:10:13 -0300

    Create color-pallet.sh

---
+++ color-pallet.sh
@@ -0,0 +1,19 @@
+# Paleta de cores
+# Da cor 0 até a 255
+AAA=$(printf "%124s"); echo ${AAA// /─}; for X in {0..255}; do printf "│\x1b[38;5;${X}mcolor%-5i\x1b[0m│\t" $X; if ! (( ($X + 1) % 8)); then echo; fi; done; AAA=$(printf "%124s"); echo ${AAA// /─}
+
+
+
+# Identado bonitinho:
+
+# Repetição de strings:
+AAA=$(printf "%124s")
+echo ${AAA// /─}
+
+for X in {0..255}; do
+  printf "│\x1b[38;5;${X}mcolor%-5i\x1b[0m│\t" $X
+  if ! (( ($X + 1) % 8)); then
+    echo
+  fi
+done
+echo ${AAA// /─}
