860fc42252fcfc488f9f718d92239ef321cb65bc
Rodric <72948934+RodricBr@users.noreply.github.com>
2022-08-05 20:09:08 -0300

    Update better-curl-status-code.sh

--- better-curl-status-code.sh
+++ better-curl-status-code.sh
@@ -1,6 +1,6 @@
 cat root-domains.com | parallel -j40 -q curl -skw "CODE: %{http_code}\t  SIZE: %{size_download}\t %{url_effective}\n" -o /dev/null
-#                  ^             ^    ^       ^^^                                                   ^^^^^^^^^^^^^      ^
-#                  |             |    |       Silent, Insecure(HTTP), Write Out                     |||||||||||||      Output to null
+#                  ^             ^    ^       ^^^                                                               ^      ^
+#                  |             |    |       Silent, Insecure(HTTP), Write Out                                 |      Output to null
 #                  |             |  Quote, so special characters are not interpreted by the shell   Prints the URL from stdout(cat)
 #   File with domains      Number of jobs (40)
                     
