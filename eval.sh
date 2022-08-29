7ded385d182b7e7efd3c7a9ed2ba25461cc7f2b6
Rodric <72948934+RodricBr@users.noreply.github.com>
2022-08-29 00:22:17 -0300

    Update eval.sh

--- eval.sh
+++ eval.sh
@@ -4,4 +4,4 @@ eval echo \$$#
 # Mesma coisa?
 # Substituição de variáveis não tem código associado, ela está na pilha de variáveis
 echo ${!#}
-#      ^^ "!" Descreve para onde o "#" aponta, que é o $3
+#      ^^ "!" Descreve para onde o "#" aponta, que é o $0 (bash/sh/zsh... etc)
