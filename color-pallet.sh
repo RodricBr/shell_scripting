bae6c948082e2d710956ff8e377980aa63814609
Rodric <72948934+RodricBr@users.noreply.github.com>
2022-09-14 15:51:22 -0300

    Added TPUT color pallet

--- color-pallet.sh
+++ color-pallet.sh
@@ -1,10 +1,10 @@
-# Paleta de cores
+# Paleta de cores / Color Pallet (One-Line)
 # Da cor 0 até a 255
 AAA=$(printf "%124s"); echo ${AAA// /─}; for X in {0..255}; do printf "│\x1b[38;5;${X}mcolor%-5i\x1b[0m│\t" $X; if ! (( ($X + 1) % 8)); then echo; fi; done; AAA=$(printf "%124s"); echo ${AAA// /─}
 
 
 
-# Identado bonitinho:
+# Identado bonitinho: (With indentation)
 
 # Repetição de strings:
 REPETI_=$(printf "%124s")
@@ -18,5 +18,9 @@ for X in {0..255}; do
 done
 echo ${REPETI_// /─}
 
-# Ou simplesmente:
+# Ou simplesmente: (Or simply...)
 #for X in {1..124}; do echo -n '─'; done
+
+
+# Printar todas as 256 cores do TPUT no terminal / Print all 256 tput colors in the terminal
+for X in {0..255}; do tput setaf $X; tput setaf $X | cat -v; echo =$X; done
