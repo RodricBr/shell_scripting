62139ba89e97a85a7ef14f76f546c4421f346b31
Rodric <72948934+RodricBr@users.noreply.github.com>
2022-11-30 18:47:03 -0300

    Update password-generator.sh

--- password-generator.sh
+++ password-generator.sh
@@ -9,3 +9,8 @@ PASS_G
 # Ou simplesmente / Or simply:
 
 PASS_G(){ tr -dc 'a-zA-Z0-9_#@.-' < /dev/urandom | head -c "${1:-14}"; echo;} && PASS_G
+
+
+# Ou exemplo interessante / Another cool example
+
+for ((;;)); do echo -ne "PASSWORD: $(tr -dc 'a-zA-Z0-9_#@.-' < /dev/urandom | head -c ${1:-14})\n"; done
