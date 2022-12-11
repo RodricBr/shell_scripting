0921e2711af7dc2a7b1242bb6246b390a7832534
Rodric <72948934+RodricBr@users.noreply.github.com>
2022-12-10 22:33:46 -0300

    Update dircolors.sh

--- dircolors.sh
+++ dircolors.sh
@@ -14,3 +14,16 @@ $ dircolors --print-database
 
 # Primeiro, gere uma cópia local das configurações de cores do dircolors:
 $ dircolors --print-database > ~/.dircolors
+
+# Edite sua lista local conforme desejado. Quando estiver satisfeito com suas escolhas, salve o arquivo.
+# Suas configurações de cores são apenas um "banco de dados" e não podem ser usadas diretamente pelo "ls",
+# mas você pode usar o dircolors para obter o shellcode que pode ser usado para definir o LS_COLORS, da seguinte forma:
+$ dircolors --bourne-shell ~/.dircolors >> ~/.bashrc
+$ source ~/.bashrc
+
+# Você também pode fazer com que o Bash resolva o .dircolors na inicialização, em vez de fazer a conversão manualmente.
+# Realisticamente, você provavelmente não mudará as cores com frequência, então isso pode ser muito agressivo,
+# mas é uma opção se você planeja mudar muito o esquema de cores.
+# No .bashrc, adicione esta regra:
+[[ -e $HOME/.dircolors ]] && eval "$(dircolors --sh $HOME/.dircolors)"
+
