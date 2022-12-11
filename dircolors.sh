bf313971633330999ee132de31b23c4c1598c88f
Rodric <72948934+RodricBr@users.noreply.github.com>
2022-12-10 22:27:48 -0300

    Update dircolors.sh

--- dircolors.sh
+++ dircolors.sh
@@ -1,3 +1,12 @@
-# Ad
-$ dircolors --print-database
+# Definir cores em sua sessão no terminal é apenas temporário.
+# Às vezes, o efeito dura algumas linhas; isso ocorre porque esse método de definir cores depende de uma instrução "printf" para definir
+# um modo que dura apenas até que algo o substitua.
+# A maneira como um emulador de terminal geralmente obtém instruções sobre quais cores usar é
+# a partir das configurações da variável de ambiente "LS_COLORS", que por sua vez é preenchida pelas configurações de dircolors.
 $ echo $LS_COLORS | tr ':' '\n'
+
+OU
+
+$ dircolors --print-database
+
+
