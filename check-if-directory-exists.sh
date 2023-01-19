6a156cb15b714f038bf8ededb8432e7f5d406434
Rodric <72948934+RodricBr@users.noreply.github.com>
2023-01-18 22:23:16 -0300

    Update check-if-directory-exists.sh

--- check-if-directory-exists.sh
+++ check-if-directory-exists.sh
@@ -2,6 +2,7 @@
 
  # Trying to cd(change directory) to aaaa, if it doesn't exist (returns error), echo... (throwing all the output and error to null)
  # Works really well - since it's a builtin -, visually simple and it's really fast
+ # As you already know, "||" is the "OR" operator. It executes our "echo" message on the right only if "cd" returned an error (exit code != 0)
  cd "aaaa" &>/dev/null || { echo "Error: directory not found." ; }
  
  # If "aaaa" is a directory (-d) returns something (using && instead of || in this case would be if it existed)
