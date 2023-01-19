da6ae6237951d1fac496670c52006be1701adb78
Rodric <72948934+RodricBr@users.noreply.github.com>
2023-01-18 22:10:32 -0300

    Update check-if-directory-exists.sh

--- check-if-directory-exists.sh
+++ check-if-directory-exists.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
  # Trying to cd(change directory) to aaaa, if it doesn't exist (returns error), echo... (throwing all the output and error to null)
- # Works really well, visually simple and it's really fast
+ # Works really well - since it's a builtin -, visually simple and it's really fast
  cd "aaaa" &>/dev/null || { echo "Error: directory not found." ; }
  
- # If "aaaa" is a file (-f) returns something (using && instead of || in this case would be if it existed)
+ # If "aaaa" is a directory (-d) returns something (using && instead of || in this case would be if it existed)
  # In this case, "||" will echo only if the exit code is not 0
- [ -f "aaaa" ]|| echo "doesn't exist"
+ [ -d "aaaa" ]|| echo "Doesn't exist"
+ [ -d "aaaa" ]&& echo "Exists" || echo "Doesn't exist"
