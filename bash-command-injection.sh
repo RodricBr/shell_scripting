9b0721a6e5c9933e37fa634537cf9ce84b719d87
Rodric <72948934+RodricBr@users.noreply.github.com>
2023-03-01 17:54:34 -0300

    Update bash-command-injection.sh

--- bash-command-injection.sh
+++ bash-command-injection.sh
@@ -4,6 +4,13 @@
 # ${0##\-}<<<$\'\\$(($((1<<1))#10011010))\\$(($((1<<1))#10100011))\'
 # Let's understand what the hell is happening here.
 
+## 1.1:
+# ${} is a parameter expansion; "$0" == is the first parameter, which is the script itself.
+# Since we're "executing" "$0" straight on the shell, the program that is getting executed is bash
+# But on my shell, bash had a little "-" dash sign next to the "b" letter of bash, so I removed it
+# using parameter expansion (${0##\-} == removes the dash "-" from "-bash", and we're left with "bash")
+
+## 1.2:
 # First of all, "$'Something\nSomething-else'" causes escape sequences to be interpreted.
 # So we can call an octal to be interpreted to text, just like so (154 in octal == l; 163 in octal == s):
 $'\154\163'
