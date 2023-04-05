d84b771ad1221d74acd0ba163fe5e099c06c24eb
Rodric <72948934+RodricBr@users.noreply.github.com>
2023-04-04 22:44:34 -0300

    Update and rename funcao_for.sh to functions.sh

---
+++ functions.sh
@@ -0,0 +1,33 @@
+# Printa cada palavra em uma linha
+palavras(){ for WORD_; do echo "$WORD_"; done; }
+
+# Resultado
+$ palavras Um Dois Tres
+Um
+Dois
+Tres
+
+# declare -f palavras
+
+#############################################################################################################################################################
+
+# Um detalhe interessante sobre funções no bash.
+# Quando declaramos uma função utilizando "function" o bash interpretará a string a seguir como o nome da função
+# Dessa forma:
+function NomeDaFuncao {  }
+
+# Note que não é necessário utilizar os parentesis, pois estamos especificamente dizendo ao shell que "NomeDaFuncao" será uma função.
+# Esse é um método "outdated", não é ideal utiliza-lo nos tempos atuais do bash
+
+# Funções nos tempos atuais:
+NomeDaFuncao(){  }
+
+# Note que dessa vez foi necessário o uso dos parentesis, ele é importante pois informa ao bash que estamos tratando de uma função.
+# Caso contrário, ele irá interpretar os "{}" como brace expansion, e retornará com erro.
+
+# Podemos testar isso dessa forma:
+NomeDaFuncao(){ echo a; }; NomeDaFuncao # Correto e com os parentesis
+NomeDaFuncao{ echo a; }; NomeDaFuncao # Errado e sem os parentesis
+
+function NomeDaFuncao { echo a; }; NomeDaFuncao # Correto sem os parentesis
+function NomeDaFuncao(){ echo a; }; NomeDaFuncao # Correto com os parentesis
