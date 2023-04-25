324a80f985c0367c5e3f8d305f3250ea3197aabc
Rodric <72948934+RodricBr@users.noreply.github.com>
2023-04-25 00:28:46 -0300

    Create undocumented-for-method.sh

---
+++ undocumented-for-method.sh
@@ -0,0 +1,10 @@
+# Undocumented method using braces:
+time for ((X=1; X<=2000; X++)) { echo $((2+2)) &>/dev/null;}
+
+# Default method the we see in many scripts:
+time for ((X=1; X<=2000; X++)); do echo $((2+2)) &>/dev/null; done
+
+# It is an optional way of writing the body of the "for" loop
+# The condition would be written differently in Bourne shell (sh).
+
+# In Bourne shell (sh) we use "do" and "done" in place of "{" and "}".
