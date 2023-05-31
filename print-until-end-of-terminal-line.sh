9d06ac599136531ef7d6b2e35f1e78c1e4566a23
Rodric <72948934+RodricBr@users.noreply.github.com>
2023-05-31 01:08:41 -0300

    Update print-until-end-of-terminal-line.sh

--- print-until-end-of-terminal-line.sh
+++ print-until-end-of-terminal-line.sh
@@ -4,6 +4,10 @@ printf %$(tput cols)s | tr " " "-"
 # OR pure bash:
 AAA_=$(printf %$(tput cols)s); echo ${AAA_// /-}
 
+# OR MORE MADNESS! :
+
+AAA_=$(printf %$(tput cols)s); { : ${AAA_// /-}; }; echo $_
+
 # Prints the number of columns
 tput cols
 
