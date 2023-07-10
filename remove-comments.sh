9335d00f3cb3a5df609cd69d95ad3dda7d8a312c
Rodric <72948934+RodricBr@users.noreply.github.com>
2023-07-10 11:56:39 -0300

    Create remove-comments.sh

---
+++ remove-comments.sh
@@ -0,0 +1,11 @@
+# Removing comments that starts with "# " (hashtag and space)
+grep -v -e '^#[[:blank:]]\|^[[:space:]]*$' Arquivo.txt
+
+# Explanation:
+
+# -v == --invert-match
+# -e == regex pattern
+
+# ^#[[:blank:]] == Starting with a "#" followed by a blank character (# )
+# \| == Grep more than one pattern/string
+# ^[[:space:]]*$ == Starts and ends with a space (removing a single empty character)
