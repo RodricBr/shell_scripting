bee09a58db0d4c54915918da91698a5f3065f237
Rodric <72948934+RodricBr@users.noreply.github.com>
2023-12-03 01:09:21 -0300

    Create column.sh

---
+++ column.sh
@@ -0,0 +1,13 @@
+# Column is a command that columnate lists.
+echo -e "This-is-an-example This-is-another-row" | column -t
+
+# "-t" determines the number of columns the input contains and create a table.
+# Columns are delimited with WHITESPACES, by default, or with the characters supplied using the -s option.
+
+# Using custom delimiter (:)
+echo -e "First\x20Row │:Second Row │:Third Row\nValues:255:1337" | column -t -s ":"
+
+# Another cool example:
+(printf "PERM LINKS OWNER GROUP SIZE MONTH DAY HH:MM/YEAR NAME\n" ; ls -l | sed 1d) | column -t
+# With colors:
+(printf "\033[32mPERM LINKS OWNER GROUP SIZE MONTH DAY HH:MM/YEAR NAME\033[00m\n" ; ls -l | sed 1d) | column -t
