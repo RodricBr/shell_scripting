00484397feec1938f5355c3a7ce866e9080351ab
Rodric <72948934+RodricBr@users.noreply.github.com>
2024-07-01 16:28:45 -0300

    Create compgen.sh
    Never added compgen to the list so I'm adding it now, because it's really handy.

---
+++ compgen.sh
@@ -0,0 +1,16 @@
+# compgen is a complementary programmable built-in.
+# The option flag "-c" shows all the commands possible on the operational system. (If ran on WSL, it'll show all .dlls, .exe, .mof, .sys and etc)
+
+$ compgen -abcdefgjksuv
+-a --> Alias
+-b --> Builtins
+-c --> Commands
+-d --> Directory
+-e --> Exported Shell Variables
+-f --> File and functions
+-g --> Groups
+-j --> Jobs
+-k --> Shell reserved words
+-s --> Services
+-u --> userAlias names
+-v --> Shell variables
