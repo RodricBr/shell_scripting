3b74b56654958fb745f2470c01e4e8f0202ad810
Rodric <72948934+RodricBr@users.noreply.github.com>
2024-09-12 20:03:13 -0300

    Update printf-set-space-amount.sh

--- printf-set-space-amount.sh
+++ printf-set-space-amount.sh
@@ -3,7 +3,14 @@
 
 # % is a format specifier special character that indicates a string should be printed.
 # The number 10 specifies that the string should be padded with spaces to a total width of 10 characters, right-aligned. Like so:
-printf "Previous%10sAfter"
+printf "Hello%10sWorld!"
 
 # Bash output:
-# Previous          After
+# Hello          World!
+
+# Following the same concept, we can use print left-aligned added a dash (-) next to the number
+# The "-10" specifies that the string should be padded with spaces to a total width of 10 characters, left-aligned.
+printf "%-10sWorld!" "Hello"
+
+# Bash output:
+# Hello     World! (Notice how the space is not 10 but rather 5, because the word "Hello" has a width of 5 characters)
