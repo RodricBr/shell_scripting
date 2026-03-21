d15cf2e77176d14f744ef4b2a424900d0a7baed4
Rodrigo <72948934+RodricBr@users.noreply.github.com>
2026-03-21 15:15:00 -0300

    Create seasons.sh

---
+++ seasons.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+HOJE=$(date +%m%d)
+
+# MMDD (monthj e day)
+INICIO_OUTONO=0320
+INICIO_INVERNO=0621
+INICIO_PRIMAVERA=0922
+INICIO_VERAO=1221
+
+# Matching the season with emojis. Fall, winter, spring and summer
+if [[ "$HOJE" -ge "$INICIO_OUTONO" && "$HOJE" -lt "$INICIO_INVERNO" ]]; then
+  ESTACAO="🍁"
+elif [[ "$HOJE" -ge "$INICIO_INVERNO" && "$HOJE" -lt "$INICIO_PRIMAVERA" ]]; then
+  ESTACAO="❄️"
+elif [[ "$HOJE" -ge "$INICIO_PRIMAVERA" && "$HOJE" -lt "$INICIO_VERAO" ]]; then
+  ESTACAO="🌼"
+else
+  ESTACAO="☀️"
+fi
+
+# Output format. date - season
+DATA="$(date +%d/%m) - "
+TEXTO="${DATA}${ESTACAO}"
+
+# Getting the width of the terminal
+COLS=$(tput cols)
+ROWS=$(tput lines)
+
+# Avoiding the emoji from breaking the string since it occupies 2 spaces
+LARGURA_VISUAL=$(( ${#DATA} + 2 ))
+
+COL_POS=$(( COLS - LARGURA_VISUAL ))
+ROW_POS=$(( ROWS - 1 ))
+
+# Adjusting the string
+tput cup $ROW_POS $COL_POS
+echo -n "$TEXTO"
